dbe1099b944cd250364d7faa442a6492f0dcb592
Valentin Gonzalez <v.gonzalez@nubelity.com>
2024-03-21 16:44:01 -0600

    adding button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import style from "./button.styles.module.css";
+
+interface IIcon {
+  src?: string;
+  alt?: string;
+}
+
+type IButtonProps =
+  | {
+      variant: "primary" | "outlined";
+      children: React.ReactNode;
+      icon?: never;
+    }
+  | {
+      variant: "link";
+      children: React.ReactNode;
+      icon?: IIcon;
+    };
+
+const Button: React.FC<IButtonProps> = (props) => {
+  const {
+    variant,
+    children,
+    icon = {
+      src: "/icons/arrow.svg",
+      alt: "go",
+    },
+  } = props;
+
+  return (
+    <button className={style[variant]}>
+      {children}
+      {icon && <img src={icon.src} alt={icon.alt} />}
+    </button>
+  );
+};
+
+export default Button;
