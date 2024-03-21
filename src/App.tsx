c414f9ec5c49e933a12d576659c28c1719d6266f
Valentin Gonzalez <v.gonzalez@nubelity.com>
2024-03-21 16:46:29 -0600

    fix eslint errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
-  console.log("test");
   return (
     <div className="App">
       <Button variant="outlined">Text</Button>
